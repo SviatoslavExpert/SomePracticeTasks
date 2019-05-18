cc0772923b9eccc57839d401ba7e9b50322c7980
Sviatoslav <s.granovskiy@gmail.com>
2019-05-18 15:51:16 +0300

    playing with setters and getters

--- CatProject-001/src/com/granovskiy/Demo.java
+++ CatProject-001/src/com/granovskiy/Demo.java
@@ -11,15 +11,34 @@ public class Demo {
         Cat cat4 = cat2;
         Cat cat5 = cat3;
         //  printing to console
-        System.out.println(cat1);
-        System.out.println(cat2);
-        System.out.println(cat3);
-        System.out.println(cat4);
-        System.out.println(cat5);
+        System.out.println("This is cat1: " + cat1);
+        System.out.println("This is cat2: " + cat2);
+        System.out.println("This is cat3: " + cat3);
+        System.out.println("This is cat4: " + cat4);
+        System.out.println("This is cat5: " + cat5);
+        System.out.println();
         //  making cat5 meow
         cat5.meow(3);
+        System.out.println();
         //  cat6 made with Factory
         Cat cat6 = create("Bob", 5);
-        System.out.println(cat6);
+        System.out.println("This is cat6: " + cat6);
+        System.out.println();
+        //  cat5
+        cat5 = Cat.of("Boy", 5);
+        System.out.println("This is cat5: " + cat5);
+        System.out.println("This is cat3: " + cat3);
+        System.out.println();
+        //  state of cat2 and cat4 changed with a setter
+        cat2.setName("Mura");
+        cat2.setAge(7);
+        System.out.println("This is cat2: " + cat2);
+        System.out.println("This is cat4: " + cat4);
+        System.out.println();
+        //  Printing objects field values using getters
+        System.out.println("This is the name of cat5: " + cat5.getName());
+        System.out.println("This is the age of cat3: " + cat3.getAge());
+        System.out.println();
+
     }
 }
