67f1cba11429e5948f08b9789be8e5b3b71a599c
Sviatoslav <s.granovskiy@gmail.com>
2019-05-18 14:00:03 +0300

    second commit

--- CatProject-001/src/com/granovskiy/Demo.java
+++ CatProject-001/src/com/granovskiy/Demo.java
@@ -2,8 +2,10 @@ package com.granovskiy;
 
 public class Demo {
     public static void main(String[] args) {
-        //  creating a new cat
+        //  creating cats
         Cat cat1 = Cat.of("Vaska", 2);
+        Cat cat2 = Cat.of("Tom", 1);
         System.out.println(cat1);
+        System.out.println(cat2);
     }
 }
