0581b85ff792becd280fdeb3d58935e5c46c5d54
Sviatoslav <s.granovskiy@gmail.com>
2019-05-19 18:52:49 +0300

    Factory class added

--- Animals/src/com/granovskiy/Cat.java
+++ Animals/src/com/granovskiy/Cat.java
@@ -1,4 +1,27 @@
 package com.granovskiy;
 
-public class Cat {
+public class Cat extends Animal {
+    //  fields
+    private String type;
+    private String color;
+    //  constructor
+    public Cat(String name, String ration, int age, double weight, boolean isSex, String type, String color) {
+        super(name, ration, age, weight, isSex);
+        this.type = type;
+        this.color = color;
+    }
+
+    @Override
+    public void getVoice() {
+        System.out.println("Meow-meow!");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "type='" + type + '\'' +
+                ", color='" + color + '\'' +
+                super.toString() +
+                '}';
+    }
 }
--- Animals/src/com/granovskiy/Chicken.java
+++ Animals/src/com/granovskiy/Chicken.java
@@ -1,4 +1,28 @@
 package com.granovskiy;
 
-public class Chicken {
+public class Chicken extends Animal {
+    //fields
+    private String size;
+    private String type;
+
+    //  constructor
+    public Chicken(String name, String ration, int age, double weight, boolean isSex, String size, String type) {
+        super(name, ration, age, weight, isSex);
+        this.size = size;
+        this.type = type;
+    }
+
+    @Override
+    public void getVoice() {
+        System.out.println("Kud-ku-da");
+    }
+
+    @Override
+    public String toString() {
+        return "Chicken{" +
+                "size='" + size + '\'' +
+                ", type='" + type + '\'' +
+                super.toString() +
+                '}';
+    }
 }
