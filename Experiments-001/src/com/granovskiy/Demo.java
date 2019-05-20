aa14d3fc0c795f3d1ba7c84fffdc9b9c0dbc4abb
Sviatoslav <s.granovskiy@gmail.com>
2019-05-20 21:32:07 +0300

    some object properties changed

--- Experiments-001/src/com/granovskiy/Demo.java
+++ Experiments-001/src/com/granovskiy/Demo.java
@@ -2,15 +2,31 @@ package com.granovskiy;
 
 public class Demo {
     public static void main(String[] args) {
+        //  operations with numbers
         int a = 10;
         int b = a;
         b++;
         System.out.println(a);   //  10
         System.out.println(b);   //  11
+        System.out.println();
+
+        //  operations with cats
+        //  original
         Cat cat = new Cat("Vaska", 3);
+        System.out.println("This is original cat's age: " + cat.age);  //  3
+        //  the second
         Cat cat2 = cat;
         cat2.age = 5;
-        System.out.println("This is cat's age: " + cat.age);  //  5
+        System.out.println("This is changed cat's age: " + cat.age);  //  5
+        //  the third
+        cat2 = doSmth(cat);
+        System.out.println("This is another changed cat's age: " + cat.age);  //  7
+        System.out.println();
+    }
+
+    private static Cat doSmth(Cat cat) {
+        cat.age = 7;
+        return cat;
     }
 
     static class Cat {
